893494afd4ee4d7044362eac851a08b52b4735e0
Sayan Snigdha Pal <sayanpalb@gmail.com>
2023-09-29 12:21:14 +0530

    CRUD functionality build done -creating database -connecting with mongoose -created model -updated controller functions

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require('colors')
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB()
 
 // middleware
 app.use(express.json());
